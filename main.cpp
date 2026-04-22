3e0144d46eae07c6b680b0a98da1a8c99bf38032
Elif Özkaradeniz <elifozkaradeniz@gmail.com>
2026-04-22 09:58:01 +0300

    W-A-S-D ile oyuncu hareketi eklendi

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ int main()
     sf::RenderWindow window(sf::VideoMode(800, 600), "Top-Down Shooter - Ilk Deneme");
 
     sf::CircleShape oyuncu(50.f);
-    oyuncu.setFillColor(sf::Color::Green);
-    oyuncu.setPosition(350.f, 250.f); // Daireyi ekranýn ortasýna koyar
+    oyuncu.setFillColor(sf::Color::Blue);
+    oyuncu.setPosition(350.f, 250.f);
 
     while (window.isOpen())
     {
@@ -16,6 +16,26 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+        float hiz = 0.5f;
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+        {
+            oyuncu.move(0.f, -hiz);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+        {
+            oyuncu.move(0.f, hiz);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            oyuncu.move(-hiz, 0.f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        {
+            oyuncu.move(hiz, 0.f);
+        }
+
 
         window.clear();
         window.draw(oyuncu);
